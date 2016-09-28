022d2cac9e989ff486d97001f15cf40fba5b5e8a
joe9 <joe9mail@gmail.com>
2016-09-28 10:53:58 -0700

    tested with a few keycodes and it works

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,13 @@
 
 module Main where
 
-import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.Maybe
 --
 import Lib
 
 main :: IO ()
-main = TIO.putStrLn (showKeySymbol (fromMaybe XK_NoSymbol (fst (getKeySymbol customDvorak 10))))
+main = TIO.putStrLn (showKeySymbol (fromMaybe XK_NoSymbol (fst (getKeySymbol customDvorak 11))))
 
 getKeySymbol :: State -> KeyCode -> (Maybe KeySymbol, State)
 getKeySymbol s k = onKeyCode k s
