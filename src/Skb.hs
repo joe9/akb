5999577f92898fd5f92f367ab0b78a994cd12bbb
joe9 <joe9mail@gmail.com>
2016-09-30 11:46:24 -0700

    remove the fibonacci tutorial stuff

--- src/Skb.hs
+++ src/Skb.hs
@@ -51,23 +51,6 @@ import State
 import Keymap.CustomDvorak
 
 -- https://wiki.haskell.org/Foreign_Function_Interface
-fibonacci :: Int -> Int
-fibonacci n = fibs !! n
-  where
-    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-fibonacci_hs :: CInt -> CInt
-fibonacci_hs = fromIntegral . fibonacci . fromIntegral
-
-foreign export ccall fibonacci_hs :: CInt -> CInt
-
-fibonacciv :: Int -> SV.Vector Int
-fibonacciv n = SV.fromList [1, 2]
-
-fibonacciv_hs :: CInt -> SV.Vector CInt
-fibonacciv_hs = SV.map fromIntegral . fibonacciv . fromIntegral
-
--- https://wiki.haskell.org/Foreign_Function_Interface
 -- http://stackoverflow.com/questions/8964362/haskell-stableptr
 -- http://stackoverflow.com/questions/14125057/how-to-poke-a-vector-or-to-get-a-ptr-vector-to-its-data
 -- https://wiki.haskell.org/Calling_Haskell_from_C
@@ -78,6 +61,7 @@ skb_state_new :: CInt -> IO (StablePtr StateIORef)
 skb_state_new i = do
   stateIORef <- newIORef (pickInitialState i)
   newStablePtr (StateIORef stateIORef)
+
 foreign export ccall skb_state_new :: CInt -> IO (StablePtr StateIORef)
 
 skb_state_key_get_one_sym :: StablePtr StateIORef -> KeyCode -> IO Word32
@@ -89,10 +73,12 @@ skb_state_key_get_one_sym ptr keycode = do
   case keySym of
     Just k -> return (unKeySymbol k)
     Nothing -> return (unKeySymbol XK_NoSymbol)
+
 foreign export ccall skb_state_key_get_one_sym :: StablePtr StateIORef -> KeyCode -> IO Word32
 
 skb_state_unref :: StablePtr StateIORef -> IO ()
 skb_state_unref = freeStablePtr
+
 foreign export ccall skb_state_unref :: StablePtr StateIORef -> IO ()
 
 pickInitialState :: CInt -> State
