c26e3b943525f48d80162fd6e40cdd6b59eb7429
joe9 <joe9mail@gmail.com>
2016-09-30 12:52:37 -0700

    split bitmask stuff from modifiers

--- src/Modifiers.hs
+++ src/Modifiers.hs
@@ -14,6 +14,8 @@ module Modifiers
 import Control.Monad
 import Data.Bits
 import Data.Word
+--
+import BitMask
 
 type Modifiers = Word32
 
@@ -44,21 +46,7 @@ data Modifier
   | Mod5
   deriving (Bounded, Enum, Eq, Show)
 
--- got this idea from
--- http://stackoverflow.com/questions/15910363/represent-a-list-of-enums-bitwise-as-an-int
-class ToBitMask a where
-  toBitMask :: a -> Word32
-  -- | Using a DefaultSignatures extension to declare a default signature with
-  -- an `Enum` constraint without affecting the constraints of the class itself.
-  default toBitMask :: Enum a =>
-    a -> Word32
-  toBitMask = shiftL 1 . fromEnum
-
 instance ToBitMask Modifier
-
-instance (ToBitMask a) =>
-         ToBitMask [a] where
-  toBitMask = foldr (.|.) 0 . map toBitMask
 
 setModifier :: Modifiers -> Modifier -> Modifiers
 -- setModifier ms = (.|.) ms . toBitMask
@@ -69,22 +57,3 @@ clearModifier ms = clearBit ms . fromEnum
 
 testModifier :: Modifiers -> Modifier -> Bool
 testModifier ms = testBit ms . fromEnum
-
--- | Not making this a typeclass, since it already generalizes over all
--- imaginable instances with help of `MonadPlus`.
-fromBitMask
-  :: (MonadPlus m, Enum a, Bounded a, ToBitMask a)
-  => Word32 -> m a
-fromBitMask bm = msum $ map asInBM $ enumFrom minBound
-  where
-    asInBM a =
-      if isInBitMask bm a
-        then return a
-        else mzero
-
-isInBitMask
-  :: (ToBitMask a)
-  => Word32 -> a -> Bool
-isInBitMask bm a =
-  let aBM = toBitMask a
-  in aBM == aBM .&. bm
