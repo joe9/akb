f917decf0148bf9e5f7727e9a19199c76096a653
joe9 <joe9mail@gmail.com>
2016-10-03 19:12:16 -0700

    formatting change

--- src/OnKeyEvent.hs
+++ src/OnKeyEvent.hs
@@ -2,10 +2,11 @@ module OnKeyEvent where
 
 import           Data.Default
 import qualified Data.Vector  as V
+
 --
-import           Modifiers
-import           State
 import KeySymbolDefinitions
+import Modifiers
+import State
 
 stickyWhatToDoWithKeySymbol :: KeySymbol -> Either ModifierMap KeySymbol
 stickyWhatToDoWithKeySymbol XKB_KEY_Control_L =
@@ -15,7 +16,7 @@ stickyWhatToDoWithKeySymbol XKB_KEY_Control_L =
        Control
        (stickyPressModifier XKB_KEY_Control_L Control)
        id -- nothing to do in sticky mode for release
-    )
+     )
 stickyWhatToDoWithKeySymbol XKB_KEY_Shift_L =
   Left
     (ModifierMap
@@ -23,7 +24,7 @@ stickyWhatToDoWithKeySymbol XKB_KEY_Shift_L =
        Shift
        (stickyPressModifier XKB_KEY_Shift_L Shift)
        id -- nothing to do in sticky mode for release
-    )
+     )
 stickyWhatToDoWithKeySymbol XKB_KEY_Alt_L =
   Left
     (ModifierMap
@@ -31,7 +32,7 @@ stickyWhatToDoWithKeySymbol XKB_KEY_Alt_L =
        Mod1
        (stickyPressModifier XKB_KEY_Alt_L Mod1)
        id -- nothing to do in sticky mode for release
-    )
+     )
 stickyWhatToDoWithKeySymbol XKB_KEY_Meta_L =
   Left
     (ModifierMap
@@ -39,7 +40,7 @@ stickyWhatToDoWithKeySymbol XKB_KEY_Meta_L =
        Mod3
        (stickyPressModifier XKB_KEY_Meta_L Mod3)
        id -- nothing to do in sticky mode for release
-    )
+     )
 stickyWhatToDoWithKeySymbol k = Right k
 
 whatToDoWithKeySymbol :: KeySymbol -> Either ModifierMap KeySymbol
@@ -49,31 +50,26 @@ whatToDoWithKeySymbol XKB_KEY_Control_L =
        XKB_KEY_Control_L
        Control
        (pressModifier XKB_KEY_Control_L Control)
-       (releaseModifier XKB_KEY_Control_L Control)
-    )
+       (releaseModifier XKB_KEY_Control_L Control))
 whatToDoWithKeySymbol XKB_KEY_Shift_L =
   Left
     (ModifierMap
        XKB_KEY_Shift_L
        Shift
        (pressModifier XKB_KEY_Shift_L Shift)
-       (releaseModifier XKB_KEY_Control_L Control)
-    )
+       (releaseModifier XKB_KEY_Control_L Control))
 whatToDoWithKeySymbol XKB_KEY_Alt_L =
   Left
     (ModifierMap
        XKB_KEY_Alt_L
        Mod1
        (pressModifier XKB_KEY_Alt_L Mod1)
-       (releaseModifier XKB_KEY_Control_L Control)
-    )
+       (releaseModifier XKB_KEY_Control_L Control))
 whatToDoWithKeySymbol XKB_KEY_Meta_L =
   Left
     (ModifierMap
        XKB_KEY_Meta_L
        Mod3
        (pressModifier XKB_KEY_Meta_L Mod3)
-       (releaseModifier XKB_KEY_Control_L Control)
-    )
+       (releaseModifier XKB_KEY_Control_L Control))
 whatToDoWithKeySymbol k = Right k
-
--- src/Skb.hs
+++ src/Skb.hs
@@ -1,20 +1,20 @@
-
 module Skb where
 
 --   http://stackoverflow.com/questions/14125057/how-to-poke-a-vector-or-to-get-a-ptr-vector-to-its-data
-import Foreign.C.Types
-import Foreign.C.String
-import Foreign.StablePtr
-import Data.Word
 import Data.Default
 import Data.IORef
---
-import KeySymbolDefinitions
-import State
-import Keymap.CustomDvorak
-import Modifiers
--- import BitMask
+import Data.Word
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.StablePtr
 
+--
+import Keymap.CustomDvorak
+import KeySymbolDefinitions
+import Modifiers
+import State
+
+-- import BitMask
 -- https://wiki.haskell.org/Foreign_Function_Interface
 -- http://stackoverflow.com/questions/8964362/haskell-stableptr
 -- http://stackoverflow.com/questions/14125057/how-to-poke-a-vector-or-to-get-a-ptr-vector-to-its-data
@@ -33,7 +33,7 @@ skb_state_key_get_one_sym ptr keycode =
     ptr
     (\state ->
        let (keySym, _, updatedState) = onKeyCode keycode state
-       in  (unKeySymbol keySym, updatedState))
+       in (unKeySymbol keySym, updatedState))
 
 withState :: StablePtr StateIORef -> (State -> (a, State)) -> IO a
 withState ptr f = do
@@ -54,78 +54,83 @@ pickInitialState _ = def
 -- skb_state_update_key :: StablePtr StateIORef -> KeyCode -> CKeyDirection -> IO StateComponent
 -- the StateComponent is not being used by wlc, so why bother
 -- returning it.
-skb_state_update_key :: StablePtr StateIORef
-                     -> KeyCode
-                     -> Word32 -- ^KeyDirection
-                     -> IO Word32 -- ^UpdatedStateComponents
-skb_state_update_key ptr keycode 0 = -- Released
+skb_state_update_key
+  :: StablePtr StateIORef
+  -> KeyCode
+  -> Word32 -- ^KeyDirection
+  -> IO Word32 -- ^UpdatedStateComponents
+skb_state_update_key ptr keycode 0 -- Released
+ =
   withState
     ptr
-    ((\(UpdatedStateComponents f,s) -> (f,s)) . onKeyCodeRelease keycode)
-skb_state_update_key ptr keycode 1 = -- Pressed
+    ((\(UpdatedStateComponents f, s) -> (f, s)) . onKeyCodeRelease keycode)
+skb_state_update_key ptr keycode 1 -- Pressed
+ =
   withState
     ptr
     (\state ->
-       let (_, UpdatedStateComponents sc, updatedState) = onKeyCodePress keycode state
+       let (_, UpdatedStateComponents sc, updatedState) =
+             onKeyCodePress keycode state
        in (sc, updatedState))
 skb_state_update_key _ _ _ = return 0
 
-skb_state_update_mask :: StablePtr StateIORef
-                      -> Word32
-                      -> Word32
-                      -> Word32
-                      -> Word32
-                      -> Word32
-                      -> Word32
-                      -> IO Word32 -- ^UpdatedStateComponents
+skb_state_update_mask
+  :: StablePtr StateIORef
+  -> Word32
+  -> Word32
+  -> Word32
+  -> Word32
+  -> Word32
+  -> Word32
+  -> IO Word32 -- ^UpdatedStateComponents
 skb_state_update_mask ptr depressedModifiers latchedModifiers lockedModifiers depressedGroup latchedGroup lockedGroup =
   withState
-        ptr
-        (\state ->
-            let newState =
-                    state
-                    { sDepressedModifiers = depressedModifiers
-                    , sLatchedModifiers = latchedModifiers
-                    , sLockedModifiers = lockedModifiers
+    ptr
+    (\state ->
+       let newState =
+             state
+             { sDepressedModifiers = depressedModifiers
+             , sLatchedModifiers = latchedModifiers
+             , sLockedModifiers = lockedModifiers
+             , sDepressedGroup = depressedGroup
+             , sLatchedGroup = latchedGroup
+             , sLockedGroup = lockedGroup
+             }
+       in (( unUpdatedStateComponents (identifyStateChanges state newState)
+           , newState)))
 
-                    , sDepressedGroup = depressedGroup
-                    , sLatchedGroup = latchedGroup
-                    , sLockedGroup = lockedGroup
-                    }
-            in ((unUpdatedStateComponents (identifyStateChanges state newState), newState)))
-
-skb_keymap_key_repeats :: Word32
-                     -> KeyCode
-                     -> Word32
+skb_keymap_key_repeats :: Word32 -> KeyCode -> Word32
 skb_keymap_key_repeats keymapIndex keycode =
-    (fromIntegral . fromEnum . findIfKeyRepeats keycode . pickInitialState . fromIntegral) keymapIndex
+  (fromIntegral .
+   fromEnum . findIfKeyRepeats keycode . pickInitialState . fromIntegral)
+    keymapIndex
 
 skb_state_serialize_state_component :: StablePtr StateIORef
-                      -> Word32
-                      -> IO Word32
+                                    -> Word32
+                                    -> IO Word32
 skb_state_serialize_state_component ptr requestedStateComponent =
   withState
-        ptr
-        (\state ->
-            let value = stateComponent state
-                         (UpdatedStateComponents requestedStateComponent)
-            in (value, state))
+    ptr
+    (\state ->
+       let value =
+             stateComponent
+               state
+               (UpdatedStateComponents requestedStateComponent)
+       in (value, state))
 
-skb_state_key_get_utf :: StablePtr StateIORef
-                      -> KeyCode
-                      -> IO Word32
-skb_state_key_get_utf ptr keyCode =
-  withState ptr (keyCodeToUTF keyCode)
+skb_state_key_get_utf :: StablePtr StateIORef -> KeyCode -> IO Word32
+skb_state_key_get_utf ptr keyCode = withState ptr (keyCodeToUTF keyCode)
 
 -- TODO would have to store modifier index in the keymap/state
-skb_keymap_mod_get_index :: Word32 -- KeyMap index used by pickInitialState
-                     -> CString
-                     -> IO Word32
+skb_keymap_mod_get_index
+  :: Word32 -- KeyMap index used by pickInitialState
+  -> CString
+  -> IO Word32
 skb_keymap_mod_get_index _ = fmap modifierIndex . peekCString
 
 -- TODO would have to store modifier index in the keymap/state
-skb_keymap_led_get_index :: Word32 -- KeyMap index used by pickInitialState
-                     -> CString
-                     -> IO Word32
+skb_keymap_led_get_index
+  :: Word32 -- KeyMap index used by pickInitialState
+  -> CString
+  -> IO Word32
 skb_keymap_led_get_index _ = fmap ledIndex . peekCString
-
