9f9835f422409514c6d40bbfa574f84d3f07ed52
joe9 <joe9mail@gmail.com>
2016-10-20 15:57:08 -0700

    formatted code

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,29 @@
 
 module Main where
 
-import           Data.Maybe
+import           Control.Concurrent.STM.TQueue
+import           Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString                  as BS
+import qualified Data.HashMap.Strict              as HashMap
+import           Data.Maybe
+import           Data.Serialize
+import           Data.Serialize.Put
+import           Data.String.Conversions
 import qualified Data.Text.IO                     as TIO
 import qualified Data.Vector                      as V
-import qualified Data.HashMap.Strict                      as HashMap
-import Data.String.Conversions
-import Data.Serialize
-import Data.Serialize.Put
+import           Data.Word
 import           GHC.Show
 import           Protolude                        hiding (State, show)
 import           System.Posix.ByteString.FilePath
 import           System.Posix.FilePath
 import           Text.Groom
-import           Data.Attoparsec.ByteString.Char8
-import           Data.Word
-import           Control.Concurrent.STM.TQueue
 
 import           BitMask
+import           Data.NineP            hiding (Directory)
 import           Network.NineP.Context hiding (File)
 import qualified Network.NineP.Context as Context
+import           Network.NineP.Error
 import           Network.NineP.Server
-import Network.NineP.Error
-import           Data.NineP hiding (Directory)
 
 import Akb
 import KeySymbolDefinitions
@@ -51,9 +51,9 @@ main = do
   case parseOnly inputParser "10,Pressed" of
     Left e -> print e
     Right (Input kcode dir) -> print (keyEvent (pickInitialState 1) kcode dir)
+
 --   let context = initializeContext {cFSItems = fsList}
 --   run9PServer context
-
 getKeySymbol :: State -> KeyCode -> KeySymbol
 getKeySymbol s k = lookupKeyCode k s
 
@@ -81,12 +81,13 @@ fsList =
     , file "/group/latched/out" -- 18
     , dir "/group/locked" -- 19
     , dir "/group/locked/out" -- 20
---     , FSItem Context.File
---        ((fileDetails  "/group/locked/out") { dRead = }) []
     ]
 
+--     , FSItem Context.File
+--        ((fileDetails  "/group/locked/out") { dRead = }) []
 dir, file :: RawFilePath -> FSItem Context
 dir name = FSItem Directory (dirDetails name) []
+
 file name = FSItem Context.File (fileDetails name) []
 
 inFileWrite
@@ -101,49 +102,48 @@ inFileWrite fid offset bs fidState me c = do
   case parseOnly inputParser bs of
     Left e -> return ((Left . OtherError) (BS.append (cs e) bs), c)
     Right (Input kcode dir) -> do
-        case keyEvent (pickInitialState 1) kcode dir of
-          (Nothing, state) ->
-            return ((Right . fromIntegral . BS.length) bs, c)
-          (Just (ks, mods), state) -> do
-            let fids = cFids c
-                kcodebs = (putWord32le kcode)
-                dirbs = (putByteString . cs . show) dir
-                ksbs = (putWord32le . unKeySymbol) ks
-                modsbs = (putWord32le mods)
-                toOut0 = BS.intercalate "," (fmap runPut [kcodebs,dirbs,ksbs,modsbs])
-                toOut5 = runPut modsbs
-            -- write to all /out read channels
-            writeToOpenChannelsOfFSItemAtIndex 0 toOut0 fids
-            -- write to all /modifiers/effective/out read channels
-            writeToOpenChannelsOfFSItemAtIndex 5 toOut5 fids
-            -- write to all /modifiers/depressed/out read channels
-            let toOut7 = (runPut . putWord32le . sDepressedModifiers) state
-            writeToOpenChannelsOfFSItemAtIndex 7 toOut7 fids
-            -- write to all /modifiers/latched/out read channels
-            let toOut9 = (runPut . putWord32le . sLatchedModifiers) state
-            writeToOpenChannelsOfFSItemAtIndex 9 toOut9 fids
-            -- write to all /modifiers/locked/out read channels
-            let toOut11 = (runPut . putWord32le . sLockedModifiers) state
-            writeToOpenChannelsOfFSItemAtIndex 11 toOut11 fids
+      case keyEvent (pickInitialState 1) kcode dir of
+        (Nothing, state) -> return ((Right . fromIntegral . BS.length) bs, c)
+        (Just (ks, mods), state) -> do
+          let fids = cFids c
+              kcodebs = (putWord32le kcode)
+              dirbs = (putByteString . cs . show) dir
+              ksbs = (putWord32le . unKeySymbol) ks
+              modsbs = (putWord32le mods)
+              toOut0 =
+                BS.intercalate "," (fmap runPut [kcodebs, dirbs, ksbs, modsbs])
+              toOut5 = runPut modsbs
+          -- write to all /out read channels
+          writeToOpenChannelsOfFSItemAtIndex 0 toOut0 fids
+          -- write to all /modifiers/effective/out read channels
+          writeToOpenChannelsOfFSItemAtIndex 5 toOut5 fids
+          -- write to all /modifiers/depressed/out read channels
+          let toOut7 = (runPut . putWord32le . sDepressedModifiers) state
+          writeToOpenChannelsOfFSItemAtIndex 7 toOut7 fids
+          -- write to all /modifiers/latched/out read channels
+          let toOut9 = (runPut . putWord32le . sLatchedModifiers) state
+          writeToOpenChannelsOfFSItemAtIndex 9 toOut9 fids
+          -- write to all /modifiers/locked/out read channels
+          let toOut11 = (runPut . putWord32le . sLockedModifiers) state
+          writeToOpenChannelsOfFSItemAtIndex 11 toOut11 fids
+          -- write to all /group/effective/out read channels
+          let toOut14 = (runPut . putWord32le . sEffectiveGroup) state
+          writeToOpenChannelsOfFSItemAtIndex 14 toOut14 fids
+          -- write to all /group/depressed/out read channels
+          let toOut16 = (runPut . putWord32le . sDepressedGroup) state
+          writeToOpenChannelsOfFSItemAtIndex 16 toOut16 fids
+          -- write to all /group/latched/out read channels
+          let toOut18 = (runPut . putWord32le . sLatchedGroup) state
+          writeToOpenChannelsOfFSItemAtIndex 18 toOut18 fids
+          -- write to all /group/locked/out read channels
+          let toOut20 = (runPut . putWord32le . sLockedGroup) state
+          writeToOpenChannelsOfFSItemAtIndex 20 toOut20 fids
+          return ((Right . fromIntegral . BS.length) bs, c)
 
-            -- write to all /group/effective/out read channels
-            let toOut14 = (runPut . putWord32le . sEffectiveGroup) state
-            writeToOpenChannelsOfFSItemAtIndex 14 toOut14 fids
-            -- write to all /group/depressed/out read channels
-            let toOut16 = (runPut . putWord32le . sDepressedGroup) state
-            writeToOpenChannelsOfFSItemAtIndex 16 toOut16 fids
-            -- write to all /group/latched/out read channels
-            let toOut18 = (runPut . putWord32le . sLatchedGroup) state
-            writeToOpenChannelsOfFSItemAtIndex 18 toOut18 fids
-            -- write to all /group/locked/out read channels
-            let toOut20 = (runPut . putWord32le . sLockedGroup) state
-            writeToOpenChannelsOfFSItemAtIndex 20 toOut20 fids
-            return ((Right . fromIntegral . BS.length) bs, c)
--- inFileWrite fid offset bs fidState me c = do
---   atomically (writeTQueue q bs)
---   return ((Right . fromIntegral . BS.length) bs, c)
-
-data Input = Input KeyCode KeyDirection deriving Show
+data Input =
+  Input KeyCode
+        KeyDirection
+  deriving (Show)
 
 inputParser :: Parser Input
 inputParser = do
@@ -154,12 +154,16 @@ inputParser = do
 
 keyDirectionParser :: Parser KeyDirection
 keyDirectionParser =
-      (string "Pressed" >> return Pressed)
-  <|> (string "Released" >> return Released)
+  (string "Pressed" >> return Pressed) <|>
+  (string "Released" >> return Released)
 
-writeToOpenChannelsOfFSItemAtIndex :: FSItemsIndex -> ByteString -> HashMap.HashMap Fid FidState -> IO ()
+writeToOpenChannelsOfFSItemAtIndex :: FSItemsIndex
+                                   -> ByteString
+                                   -> HashMap.HashMap Fid FidState
+                                   -> IO ()
 writeToOpenChannelsOfFSItemAtIndex i bs =
-  mapM_ (\f -> writeToMaybeQueue (fidQueue f) bs) . HashMap.filter (\f -> i == fidFSItemsIndex f && isJust (fidQueue f))
+  mapM_ (\f -> writeToMaybeQueue (fidQueue f) bs) .
+  HashMap.filter (\f -> i == fidFSItemsIndex f && isJust (fidQueue f))
 
 writeToMaybeQueue :: Maybe (TQueue ByteString) -> ByteString -> IO ()
 writeToMaybeQueue (Nothing) _ = return ()
